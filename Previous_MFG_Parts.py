6554570702e46a76c9f7ed672ebf8286b10dd6f9
Josh <joshowery@gmail.com>
2025-06-23 15:16:06 -0400

    Swapping over to Radwell instead of Ebay for searching

--- Previous_MFG_Parts.py
+++ Previous_MFG_Parts.py
@@ -3,44 +3,42 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import re
-
+import time, random
 driver = webdriver.Chrome()  # Selecting which search tool to use Google Chrome or Firefox, Edge, etc.
 
 
 # Prompting the user what they want to look for
 item = input("What are you looking for?")
-ebay_url = f"https://www.ebay.com/sch/i.html?_nkw={item.replace(' ', '+')}" # <--- Ebay's URL, replacing spaces with '+'s
+
+ebay_url = f"https://www.radwell.com/Search/Advanced?PartNumber={item.replace(' ', '+')}" # <--- Radwell's URL, replacing spaces with '+'s
 driver.get(ebay_url)
+
+
+
 
 # Giving some time for the search tool to load
 wait = WebDriverWait(driver, 10) # Function Parameters (Driver, time in seconds)
-wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.s-item")))
+wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table tbody tr")))
 
 #Storing the results
-search_results = driver.find_elements(By.CSS_SELECTOR, "li.s-item")[:3] # <--- List indexing for the first 3 items
+search_results = driver.find_elements(By.CSS_SELECTOR, "table tbody tr")[:3] # <--- List indexing for the first 3 items
 
 
 # For each search result we need to extract the name, brand, and price
 listings = []
+
+
 for search in search_results:
-    title_elems = search.find_elements(By.CSS_SELECTOR, ".s-item__title")
-    title = title_elems[0].text if title_elems else "Unknown"
-    # On ebay, the "brand" is often in the "data-brand" attribute or inside a span:
+    time.sleep(random.uniform(2, 5))
 
-    brand_elems = search.find_elements(By.CSS_SELECTOR, ".s-item__subtitle--tagblock")
+    name = search.find_element(By.CSS_SELECTOR, "td:nth-child(1) a").text
+    brand = search.find_element(By.CSS_SELECTOR, "td:nth-child(2)").text
+    price = search.find_element(By.CSS_SELECTOR, "td:nth-child(6)").text
+    link = search.find_element(By.CSS_SELECTOR, "td:nth-child(1) a").get_attribute("href")
 
-    brand = brand_elems[0].text if brand_elems else "Unknown"
-
-    # price sometimes split into whole + fraction// only on amazon not on ebay
-    price = search.find_element(By.CSS_SELECTOR, ".s-item__price").text
-
-    listings.append({"name": title, "brand": brand, "price": price}) # Each listing entry will use a dictionary to store the price, brand, and name
-
-    #For troubleshooting incorporating
+    listings.append({"name": name, "brand": brand, "price": price, "link": link})
 
 for idx, item in enumerate(listings, 1):
-    print(f"{idx})\n {item['brand']}\n {item['price']}\n")
-    print()
+    print(f"{idx})\n {item['brand']}\n {item['price']}\n Link: {item['link']}")
 
 driver.quit()

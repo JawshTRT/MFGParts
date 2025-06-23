173d8433f33e11ca045be9170eaa2ac8a18426db
Josh <joshowery@gmail.com>
2025-06-23 14:37:52 -0400

    Switching back to web scraping on ebay for now to avoid having to come up with anti-web scraping tactics for websites like radwell. (cherry picked from commit 25040a0f8e42a29da1b0aba989cabd21d5490864)

--- Previous_MFG_Parts.py
+++ Previous_MFG_Parts.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,69 +8,39 @@ import time, random
 driver = webdriver.Chrome()  # Selecting which search tool to use Google Chrome or Firefox, Edge, etc.
 
 
-
-
-
-
 # Prompting the user what they want to look for
 item = input("What are you looking for?")
-
-ebay_url = f"https://www.radwell.com/Search/Advanced?PartNumber={item.replace(' ', '+')}" # <--- Radwell's URL, replacing spaces with '+'s
+ebay_url = f"https://www.ebay.com/sch/i.html?_nkw={item.replace(' ', '+')}" # <--- Ebay's URL, replacing spaces with '+'s
 driver.get(ebay_url)
 
-
-
-
 # Giving some time for the search tool to load
-wait = WebDriverWait(driver, 20) # Function Parameters (Driver, time in seconds)
-q = wait.until(EC.presence_of_element_located((By.NAME, "q")))
-q.send_keys(f"site:radwell.com {item}")
-q.send_keys(Keys.RETURN)
+wait = WebDriverWait(driver, 10) # Function Parameters (Driver, time in seconds)
+wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.s-item")))
+
+#Storing the results
+search_results = driver.find_elements(By.CSS_SELECTOR, "li.s-item")[:3] # <--- List indexing for the first 3 items
 
 
-#wait for results links to load
-wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div#search a")))
-
-
-#Only collecting links from radwell.com hrefs
-all_links = driver.find_elements(By.CSS_SELECTOR, "div#search a")
-
-
-radwell_links = []
-for link in all_links:
-    href = link.get_attribute("href")
-    if href and "radwell.com" in href:
-        radwell_links.append(href)
-
-    # Breaking out of the loop as soon as the size of the list is greater than 3
-    if len(radwell_links) >= 3:
-        break
-
-
-
+# For each search result we need to extract the name, brand, and price
 listings = []
-    #-- 2. Visit each top-3 Radwell result and scape------
-for link in radwell_links:
-    driver.get(link)
-    # Try accepting the cookies if present
-    try:
-        driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
-    except NoSuchElementException:
-        pass
+for search in search_results:
+    title_elems = search.find_elements(By.CSS_SELECTOR, ".s-item__title")
+    title = title_elems[0].text if title_elems else "Unknown"
+    # On ebay, the "brand" is often in the "data-brand" attribute or inside a span:
 
-    # wait for the table rows
-    wait.until(EC.presence_of_element_located((By.XPATH, "//table//tbody//tr")))
-    row = driver.find_element(By.XPATH, "(//table//tbody//tr)[1]")
-    name = link.find_element(By.CSS_SELECTOR, "td:nth-child(1) a").text
-    brand = link.find_element(By.CSS_SELECTOR, "td:nth-child(2)").text
-    price = link.find_element(By.CSS_SELECTOR, "td:nth-child(6)").text
-    listings.append({"name": name, "brand": brand, "price": price})
+    brand_elems = search.find_elements(By.CSS_SELECTOR, ".s-item__subtitle--tagblock")
 
+    brand = brand_elems[0].text if brand_elems else "Unknown"
 
+    # price sometimes split into whole + fraction// only on amazon not on ebay
+    price = search.find_element(By.CSS_SELECTOR, ".s-item__price").text
 
-driver.quit()
+    listings.append({"name": title, "brand": brand, "price": price}) # Each listing entry will use a dictionary to store the price, brand, and name
+
+    #For troubleshooting incorporating
 
 for idx, item in enumerate(listings, 1):
-    print(f"{idx})\n {item['brand']}\n {item['price']}\n Link: {item['link']}")
+    print(f"{idx})\n {item['brand']}\n {item['price']}\n")
+
 
 driver.quit()

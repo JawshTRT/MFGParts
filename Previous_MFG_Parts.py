20118e13a520b5721bd34064fae9fe8e4a835a3c
Josh <joshowery@gmail.com>
2025-06-23 14:35:20 -0400

    Making more changes to wholy stick to searching on ebay first, then other sites later (may consider using a class-based structure)

--- Previous_MFG_Parts.py
+++ Previous_MFG_Parts.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
 
 driver = webdriver.Chrome()  # Selecting which search tool to use Google Chrome or Firefox, Edge, etc.
 
@@ -14,30 +15,25 @@ driver.get(ebay_url)
 
 # Giving some time for the search tool to load
 wait = WebDriverWait(driver, 10) # Function Parameters (Driver, time in seconds)
-wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.s-main-slot div[data-component-type='s-search-result']")))
+wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li.s-item")))
 
 #Storing the results
-search_results = driver.find_elements(By.CSS_SELECTOR, "div.s-main-slot div[data-component-type='s-search-result']")
+search_results = driver.find_elements(By.CSS_SELECTOR, "li.s-item")[:3] # <--- List indexing for the first 3 items
 
 
 # For each search result we need to extract the name, brand, and price
 listings = []
 for search in search_results:
-    title = search.find_elements(By.CSS_SELECTOR, "h2 a span").text
-    # On Amazon, the "brand" is often in the "data-brand" attribute or inside a span:
-    try:
-        brand = search.get_attribute("data brand") or \
-                search.find_element(By.CSS_SELECTOR, ".a-row .a-size-base").text
-    except:
-        brand = "_"
+    title_elems = search.find_elements(By.CSS_SELECTOR, ".s-item__title")
+    title = title_elems[0].text if title_elems else "Unknown"
+    # On ebay, the "brand" is often in the "data-brand" attribute or inside a span:
 
-    # price sometimes split into whole + fraction:
-    try:
-        whole = search.find_element(By.CSS_SELECTOR, ".a-row .a-size-base").text
-        frac = search.find_element(By.CSS_SELECTOR, ".a-price-fraction").text
-        price = f"${whole}{frac}"
-    except:
-        price = "_"
+    brand_elems = search.find_elements(By.CSS_SELECTOR, ".s-item__subtitle--tagblock")
+
+    brand = brand_elems[0].text if brand_elems else "Unknown"
+
+    # price sometimes split into whole + fraction// only on amazon not on ebay
+    price = search.find_element(By.CSS_SELECTOR, ".s-item__price").text
 
     listings.append({"name": title, "brand": brand, "price": price}) # Each listing entry will use a dictionary to store the price, brand, and name
 

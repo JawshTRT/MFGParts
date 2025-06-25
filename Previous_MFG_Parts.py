6e0c227c2087bfc06fe4da5dfd73a599156d7268
Josh <joshowery@gmail.com>
2025-06-25 09:11:56 -0400

    Made changes to URL function, to incorporate strategies to better pick up search results, properly determine if a link is a viable product page

--- Previous_MFG_Parts.py
+++ Previous_MFG_Parts.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
@@ -22,18 +23,30 @@ def URL_Fetcher(browser):
     :return:
         A list of 7 links to sift through items and their specifications
     """
-    Link = browser.find_elements(By.CSS_SELECTOR, ".s-item__link")[:7] # <--- List indexing for the first 3 items skipping the first 2 because they appear to be ghost links?
-
+    Link = browser.find_element(By.CSS_SELECTOR, "ul.srp-results").find_elements(By.CSS_SELECTOR, "li.s-item") # <--- List indexing for the first 3 items skipping the first 2 because they appear to be ghost links?
+    CleanLinks = []
+    seen_urls = set()
     # Iterating through links to see if there are any invalid links
     for l in Link:
-        href = l.get_attribute("href")
-        # Only product pages have '/itm/' in their path
-        if href and "/itm/" not in urlparse(href).path:
-            # If the link does not have /itm/ then remove the weblink and append the list a new link
-            Link.pop(0)
-            Link.append(browser.find_elements(By.CSS_SELECTOR, ".s-item__link")[len(Link):len(Link) + 1])
 
-    return Link
+        try:
+            href = l.find_element(By.CSS_SELECTOR, "a.s-item__link").get_attribute("href")
+            title = l.find_element(By.CSS_SELECTOR, ".s-item__title").text
+            price = l.find_element(By.CSS_SELECTOR, ".s-item__price").text
+        except NoSuchElementException:
+            continue
+        if "/itm/" not in href:
+            continue
+
+        if href in seen_urls:
+            continue
+        seen_urls.add(href)
+
+        CleanLinks.append(l)
+        if len(CleanLinks) == 12:
+            break
+    return CleanLinks
+
 def get_top_3_ebay(item_query):
     driver = webdriver.Chrome()  # Selecting which search tool to use Google Chrome or Firefox, Edge, etc.
     # Prompting the user what they want to look for
@@ -45,37 +58,33 @@ def get_top_3_ebay(item_query):
     # Giving some time for the search tool to load
     wait = WebDriverWait(driver, 10)  # Function Parameters (Driver, time in seconds)
     wait.until(EC.presence_of_all_elements_located(
-        (By.CSS_SELECTOR, ".s-item__link")))
+        (By.CSS_SELECTOR, "ul.srp-results"))) # <-- only shows up once ebay has rendered actual search hits
 
 
     # Storing the first 3 results via list slicing
     search_results = URL_Fetcher(driver)  # <--- List indexing for the first 3 items skipping the first 2 because they appear to be ghost links?
 
-    # For each search result we need to extract the name, brand, and price
+
 
     listings = []
     for search in search_results:
 
+
+        # For each search result we need to extract the name, brand, and price
         name = search.find_element(By.CSS_SELECTOR, ".s-item__title").text
         # On ebay, the "brand" is often in the "data-brand" attribute or inside a span:
 
         #Brand often in the subtitle block but may not always exist
-        try:
-            brand = search.find_element(By.CSS_SELECTOR, ".s-item__seller-info-text").text
-        except:
-            brand = "Unknown"
+        cond = search.find_element(By.CSS_SELECTOR, ".s-item__subtitle").text
 
-    # Price sometimes split into whole + fraction// only on amazon not on ebay
+        # Price sometimes split into whole + fraction// only on amazon not on ebay
 
-        try:
-            price = search.find_element(By.CSS_SELECTOR, ".s-item__price").text
 
-        except:
-            price = "N/A"
+        price = search.find_element(By.CSS_SELECTOR, ".s-item__price").text
 
         link = search.find_element(By.CSS_SELECTOR, ".s-item__link").get_attribute("href")
 
-        listings.append({"name": name, "brand": brand,
+        listings.append({"name": name, "cond": cond,
                      "price": price, "link": link})  # Each listing entry will use a dictionary to store the price, brand, and name
 
     driver.quit()
@@ -87,5 +96,5 @@ if __name__ == "__main__":
     item = input("Enter an item to search on ebay: ")
     top3 = get_top_3_ebay(item)
     for i, listing in enumerate(top3, 1):
-        print(f"{i})  Title: [{listing['name']}]\n Seller: [{listing['brand']}]\n Price: [{listing['price']}]\n URL: [{listing["link"]}]\n\n")
+        print(f"{i})  Title: [{listing['name']}]\n Condition: [{listing['cond']}]\n Price: [{listing['price']}]\n URL: [{listing["link"]}]\n\n")
 

a96118ab3bf31b6e7e59e6d339cb063cd07372d5
Josh Howery <joshowery@gmail.com>
2025-06-27 15:51:39 -0400

    Developed a more robust workaround bot-checkers, if no listings are found, the driver will just restart and reattempt to fetch a search query that retrieves listings, also started trying running in headless

--- MFGScraper.py
+++ MFGScraper.py
@@ -10,7 +10,15 @@ import locale  # <---- currency formatting and conversion
 
 import time
 
-
+def Driver_Init():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless") #<---- Considering running headless
+    # Selecting which search tool to use Google Chrome or Firefox, Edge, etc.
+    driver = webdriver.Chrome(options=options)
+    stealth(driver, vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.", renderer=
+    "Intel Iris OpenGL Engine",
+            fix_hairline=True)
+    return driver
 def USD_Convert(price):
     """
     Converts any non USD price to USD
@@ -26,21 +34,35 @@ def ImportCSv(filename):
     # Extracting the file column full of item names
     search_terms = df['Name'].dropna().astype(str).tolist()
     return search_terms
-def URL_Fetcher(browser):
+def URL_Fetcher(browser, item_query):
     """
     Checks a url to determine if it is a valid item page on the URL...
+    :param item_query:
+        Item to be searched through ebay
     :param browser:
         The driver object to access the webpage
     :return CleanLinks:
         A list of 7 links to sift through items and their specifications
     """
+    ebay_url = f"https://www.ebay.com/sch/i.html?_nkw={item_query.replace(' ', '+')}"  # <--- Ebay's URL, replacing spaces with '+'s
+    browser.get(ebay_url)
+    wait = WebDriverWait(browser, 20)  # Waiting even longer (10 more seconds for the search results list to exit)
+    wait.until(EC.presence_of_all_elements_located(
+        (By.CSS_SELECTOR, "ul.srp-results")))  # <-- only shows up once ebay has rendered actual search hits
+    WebDriverWait(browser, 20).until(lambda d: d.execute_script("return document.readyState") == "complete")
+
+    # Results are sometimes lazy loaded so scroll at least once
+    browser.execute_script("window.scrollBy(0, 1000);")
+    time.sleep(1)
 
     Link = browser.find_element(By.CSS_SELECTOR, "ul.srp-results").find_elements(By.CSS_SELECTOR, "li.s-item")
-    #    if len(Link) > 0: #<------ will stall if there are no results
-    #        break
+
+
     CleanLinks = []
     seen_urls = set()
-    print(len(Link))  # <---- Debug
+    print(f"{len(Link)} listings found")  # <---- Debug
+
+
     # Iterating through links to see if there are any invalid links
     for l in Link:
 
@@ -63,13 +85,6 @@ def URL_Fetcher(browser):
             break
     return CleanLinks
 def get_top_3_ebay(item_query):
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(
-        options=options)  # Selecting which search tool to use Google Chrome or Firefox, Edge, etc.
-
-    stealth(driver, vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.", renderer=
-    "Intel Iris OpenGL Engine",
-            fix_hairline=True)
 
     driver = Driver_Init()
     # Storing the results in the function URL_Fetcher function
@@ -80,20 +95,22 @@ def get_top_3_ebay(item_query):
             driver = Driver_Init()
             try:
                 search_results = URL_Fetcher(driver, item_query)
-            finally:
-                driver.quit()
-            if len(search_results) > 0:
-                break
-            print(f"No results, retrying in {2}s...")
+                if len(search_results) > 0:
+                    break
+            except Exception as e:
+                print(f"No results, retrying in {2}s...")
             time.sleep(2)
-    
+
 
     listings = []
     for search in search_results:
         # Fetching name
         name = search.find_element(By.CSS_SELECTOR, ".s-item__title").text
-        # Title
-        cond = search.find_element(By.CSS_SELECTOR, ".s-item__subtitle").text
+        # Condition
+        try:
+            cond = search.find_element(By.CSS_SELECTOR, ".s-item__subtitle").text
+        except NoSuchElementException:
+            cond = "Invalid Condition element"
         # Price
         price = search.find_element(By.CSS_SELECTOR, ".s-item__price").text
         # Link
@@ -107,7 +124,6 @@ def get_top_3_ebay(item_query):
     return listings
     # For troubleshooting incorporating a link to the webpage
 
-
 if __name__ == "__main__":
     products = ImportCSv('Sample Parts List - Sheet1.csv')
     results = []
@@ -115,4 +131,4 @@ if __name__ == "__main__":
         top_items = get_top_3_ebay(item)
         for rank, top_item in enumerate(top_items, start =1):
             results.append(top_item)
-            print(f"{rank}. Name: [{top_item['name']}]\n Cond: [{top_item['brand']}]Price: [{top_item['price']}]\n Link: [{top_item['link']}]")
+            print(f"{rank}. Name: [{top_item['name']}]\n Condition: [{top_item['cond']}]\nPrice: [{top_item['price']}]\n Link: [{top_item['link']}]")

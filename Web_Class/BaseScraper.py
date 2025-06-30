b5f689a1d0358af2099acbfb8ade5178b185570e
Josh Howery <joshowery@gmail.com>
2025-06-30 12:50:25 -0400

    Changing base abstract class methods to be outside of the default constructor

--- Web_Class/BaseScraper.py
+++ Web_Class/BaseScraper.py
@@ -19,43 +19,43 @@ class BaseScraper(ABC):
         if headless:
             options.add_argument("--headless")
 
-        @abstractmethod
-        def get_search_url(self, query: str) -> str:
-            """Build the URL to load for a given search query"""
-            pass
+    @abstractmethod
+    def get_search_url(self, query: str) -> str:
+        """Build the URL to load for a given search query"""
+        pass
 
-        @abstractmethod
-        def select_result_items(self):
-            """Return a list of WebElements pointing at each result container"""
-            pass
+    @abstractmethod
+    def select_result_items(self):
+        """Return a list of WebElements pointing at each result container"""
+        pass
 
-        @abstractmethod
-        def parse_item(self, element) -> dict:
-            """Parse a WebElement and return a dictionary
+    @abstractmethod
+    def parse_item(self, element) -> dict:
+        """Parse a WebElement and return a dictionary
 
-            """
-            pass
+        """
+        pass
 
-        def scrape(self, search_query: str, n: int = 3) -> list[dict]:
-            """
+    def scrape(self, search_query: str, n: int = 3) -> list[dict]:
+        """
 
-            :param search_query:
-                The item to search for
-            :param n:
-                Determines the number of results to grab from the search listings
-            :return results:
-            A list of dictionaries containing the search results
-            """
-            url = self.get_search_url(search_query)
-            self.driver.get(url)
+        :param search_query:
+            The item to search for
+        :param n:
+            Determines the number of results to grab from the search listings
+        :return results:
+        A list of dictionaries containing the search results
+        """
+        url = self.get_search_url(search_query)
+        self.driver.get(url)
 
-            self.driver.implicitly_wait(2)
+        self.driver.implicitly_wait(2)
 
-            elems = self.select_result_items()
-            results = [self.parse_item(el) for el in elems]
-            return results
+        elems = self.select_result_items()
+        results = [self.parse_item(el) for el in elems]
+        return results
 
-        def close(self):
-            self.driver.quit()
+    def close(self):
+        self.driver.quit()
 
 
